b68d29d566591a33eacbf66fab2b9bf1b428fda7
Adrien <adrien@arccoza.com>
2017-11-07 18:38:18 +0900

    Update bench

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var {zenhand} = require('./lib/index')
+var {zenhand, toStyleStr} = require('./lib/index')
 var print = console.log.bind(console)
 
 var start = process.hrtime()
@@ -7,3 +7,13 @@ for (var i = 0; i < 100000; i++)
 print(process.hrtime(start))
 
 print(obj)
+
+var start = process.hrtime()
+for (var i = 0; i < 100000; i++)
+  var str = toStyleStr({
+    background: 'red',
+    color: 'black',
+  })
+print(process.hrtime(start))
+
+print(str)
