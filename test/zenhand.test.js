cfef49b86a7b9186ba26fa600063872f0a566890
Adrien <adrien@arccoza.com>
2017-11-09 11:50:19 +0900

    Add `zenhand` full test

--- test/zenhand.test.js
+++ test/zenhand.test.js
@@ -30,6 +30,19 @@ let fix = {
       kebab: 'position:  absolute; background-color:  #ff0000;',
       camel: 'position:  absolute; backgroundColor:  #ff0000;',
     }
+  },
+  obj: {
+    tag: 'div',
+    attrs: {
+      class: [ 'foo', 'bar' ],
+      style: {
+        backgroundColor: '#ff0000',
+        position: 'absolute',
+        left: 'calc(1vw - 10px)',
+      },
+      id: 'ident',
+      'data-name': 'temp'
+    }
   }
 }
 
@@ -64,6 +77,21 @@ to safely convert between camel-case and kebab-case and manage whitespace.`, fun
   t.deepEqual(r = fromStyleStr(fix.style.fromStr.kebab, 'kebab', 'camel'), fix.style.obj.camel)
   t.deepEqual(r = fromStyleStr(fix.style.fromStr.kebab, 'kebab', 'kebab'), fix.style.obj.kebab)
   // t.comment(JSON.stringify(r))
+
+  t.end()
+})
+
+test(`fromStyleStr should return an obj with style properties, \
+when supplied with an inline CSS style string. Also should be able \
+to safely convert between camel-case and kebab-case and manage whitespace.`, function (t) {
+  var r
+  r = zenhand('div#ident.foo.bar[style=background-color:#ff0000;position:absolute;left:calc(1vw - 10px)][data-name=temp]',
+    {changeStyleCase: true})
+
+  t.deepEqual(r, fix.obj)
+  console.log(r)
+
+
 
   t.end()
 })
